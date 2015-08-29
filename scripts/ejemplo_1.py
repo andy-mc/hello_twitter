3508d9861675c79cc10a9962a74c0ea968a10a4d
longShot-88 <asmz5488@gmail.com>
2015-08-29 01:44:17 -0500

    Update ejemplo_1.py

--- scripts/ejemplo_1.py
+++ scripts/ejemplo_1.py
@@ -8,17 +8,17 @@ import json
 
 def oauth_login():
 	# credentials for OAuth
-	CONSUMER_KEY = ' ---- '
-	CONSUMER_SECRET =' ---- '
-	OAUTH_TOKEN = ' ---- '
-	OAUTH_TOKEN_SECRET = ' ---- '
+    CONSUMER_KEY = ' ---- '
+    CONSUMER_SECRET =' ---- '
+    OAUTH_TOKEN = ' ---- '
+    OAUTH_TOKEN_SECRET = ' ---- '
 	
 	# Creating the authentification
-	auth = twitter.oauth.OAuth( OAUTH_TOKEN, OAUTH_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET )
+    auth = twitter.oauth.OAuth( OAUTH_TOKEN, OAUTH_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET )
 	
 	# Twitter instance
-	twitter_api = twitter.Twitter(auth=auth)
-	return twitter_api
+    twitter_api = twitter.Twitter(auth=auth)
+    return twitter_api
 
 # LogIn
 twitter_api = oauth_login()
